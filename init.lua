3d7a761f6222d8129ea316fac1141d7260961883
Ravinder <ravindj@thoughtworks.com>
2018-10-06 08:10:08 +0530

    Add wifi configuration to adjust sound with different wifi connections

--- init.lua
+++ init.lua
@@ -36,3 +36,24 @@ hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Up", function()
   f.h = max.h
   win:setFrame(f)
 end)
+
+wifiWatcher = nil
+homeSSID = "u39/12 tf"
+lastSSID = hs.wifi.currentNetwork()
+
+function ssidChangedCallback()
+    newSSID = hs.wifi.currentNetwork()
+
+    if newSSID == homeSSID and lastSSID ~= homeSSID then
+        -- We just joined our home WiFi network
+        hs.audiodevice.defaultOutputDevice():setVolume(50)
+    elseif newSSID ~= homeSSID and lastSSID == homeSSID then
+        -- We just departed our home WiFi network
+        hs.audiodevice.defaultOutputDevice():setVolume(0)
+    end
+
+    lastSSID = newSSID
+end
+
+wifiWatcher = hs.wifi.watcher.new(ssidChangedCallback)
+wifiWatcher:start()
