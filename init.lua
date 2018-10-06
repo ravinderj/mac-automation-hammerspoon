be8d0820d7aa1ec241bfd53e33de82855643bd9c
Ravinder <ravindj@thoughtworks.com>
2018-10-06 08:20:25 +0530

    Add ClipboardTool spoon to manage clipboard history.

--- init.lua
+++ init.lua
@@ -4,3 +4,9 @@ require "wifiWatcher"
 hs.hotkey.bind({"cmd", "alt", "ctrl"}, "R", function()
   hs.reload()
 end)
+
+hs.loadSpoon("ClipboardTool")
+spoon.ClipboardTool:start()
+spoon.ClipboardTool:bindHotkeys({
+  toggle_clipboard = { { "ctrl","shift" }, "v" }
+})
